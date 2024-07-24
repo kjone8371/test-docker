d3d0474f8ef9b3c8e76103b8453f1519a255818d
kjone8371 <kjone8371@gmail.com>
2024-07-24 15:30:19 +0900

    유저 컨트롤러

---
+++ src/main/java/com/kjone/testdocker/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.kjone.testdocker.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+}
